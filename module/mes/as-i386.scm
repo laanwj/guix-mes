77dfdd44b77fb6eeb668c4a88231c9e26f05defc
Jan Nieuwenhuizen <janneke@gnu.org>
2017-06-13 20:31:03 +0200

    mescc: Remove jump calculation, use labels: rename jumps.
    * module/mes/as-i386.mes (i386:jump, i386:jump-byte-z, i386:jump-g,
      i386:jump-ge, i386:jump-nc, i386:jump-ncz, i386:jump-nz,
      i386:jump-z): Rename from i386:jump-label*.
    * module/mes/as-i386.scm (mes): Update exports.
    * module/language/c99/compiler.mes (ast->info): Update callers.

--- module/mes/as-i386.scm
+++ module/mes/as-i386.scm
@@ -80,12 +80,12 @@
             i386:label-mem->accu
             i386:label-mem->base
             i386:jump
-            i386:jump-label
-            i386:jump-label-byte-z
-            i386:jump-label-g
-            i386:jump-label-ge
-            i386:jump-label-nz
-            i386:jump-label-z
+            i386:jump
+            i386:jump-byte-z
+            i386:jump-g
+            i386:jump-ge
+            i386:jump-nz
+            i386:jump-z
             i386:local->accu
             i386:local->base
             i386:local-add
