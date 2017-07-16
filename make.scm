3ae27f52e4f51a140d776f0a7a328d9136ba4ea0
Jan Nieuwenhuizen <janneke@gnu.org>
2017-07-16 20:54:59 +0200

    mescc: Tinycc support: union.
    * scaffold/tests/73-union.c: New file.
    * module/language/c99/compiler.mes (union->type-entry): New
      function.
      (decl->info): Support unions.

--- make.scm
+++ make.scm
@@ -151,7 +151,8 @@ exec ${GUILE-guile} --no-auto-compile -L . -L guile -C . -C guile -s "$0" ${1+"$
  add-scaffold-test
  '("70-printf"
    "71-struct-array"
-   "72-typedef-struct-def"))
+   "72-typedef-struct-def"
+   "73-union"))
 
 (add-target (group "check-scaffold-tests/7" #:dependencies (filter (target-prefix? "check-scaffold/tests/7") %targets)))
 
---
+++ scaffold/tests/73-union.c
@@ -0,0 +1,50 @@
+/* -*-comment-start: "//";comment-end:""-*-
+ * Mes --- Maxwell Equations of Software
+ * Copyright © 2017 Jan Nieuwenhuizen <janneke@gnu.org>
+ *
+ * This file is part of Mes.
+ *
+ * Mes is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Mes is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "30-test.i"
+#include <stdio.h>
+
+typedef union foo
+{
+  char c;
+  int i;
+  void *p;
+} foo;
+
+typedef union
+{
+  char c;
+  int i;
+  void *p;
+} bar;
+
+
+int
+test ()
+{
+  union foo f = {48};
+  printf ("f.i=%d\n", f.i);
+  printf ("f.c=%c\n", f.c);
+
+  if (f.i != 48) return 1;
+  if (f.c != '0') return 1;
+
+  return 0;
+}
