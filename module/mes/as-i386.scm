accf4159a4f5bb5854ed256950209324ea947295
Jan Nieuwenhuizen <janneke@gnu.org>
2017-07-24 08:36:43 +0200

    mescc: Tinycc support: multi-byte [local] offsets.
    * stage0/x86.M1: Add 32bit variants for all 8bit instructions.
    * module/mes/as-i386.mes: Use them, switch on size.

--- module/mes/as-i386.scm
+++ module/mes/as-i386.scm
@@ -28,59 +28,56 @@
   #:use-module (mes guile)
   #:use-module (mes as)
   #:export (
-            i386:accu-cmp-value
+            i386:accu%base
+            i386:accu*base
+            i386:accu+accu
+            i386:accu+base
+            i386:accu+value
             i386:accu->base
             i386:accu->base-address
             i386:accu->base-address+n
             i386:accu->label
             i386:accu->local
-            i386:accu-mem-add
-            i386:accu-test
-            i386:accu-zero?
-            i386:accu+accu
-            i386:accu+base
-            i386:accu+value
-            i386:accu/base
-            i386:accu%base
-            i386:accu*base
-            i386:accu-base
-            i386:accu-shl
             i386:accu-and-base
+            i386:accu-base
+            i386:accu-cmp-value
+            i386:accu-mem-add
             i386:accu-negate
             i386:accu-not
             i386:accu-or-base
+            i386:accu-shl
+            i386:accu-test
             i386:accu-xor-base
+            i386:accu-zero?
+            i386:accu/base
+            i386:accu<->stack
             i386:accu<<base
             i386:accu>>base
-            i386:base-sub
+            i386:base+value
             i386:base->accu
             i386:base->accu-address
-            i386:byte-accu->base-address
             i386:base->label
             i386:base->local
+            i386:base-address->accu-address
+            i386:base-mem+n->accu
             i386:base-mem->accu
-            i386:byte-base-sub
+            i386:base-sub
+            i386:byte-accu->base-address
             i386:byte-base->accu-address
             i386:byte-base->accu-address+n
             i386:byte-base-mem->accu
-            i386:local-address->accu
+            i386:byte-base-sub
             i386:byte-local->accu
             i386:byte-local->base
             i386:byte-mem->accu
-            i386:base-mem+n->accu
             i386:byte-mem->base
-            i386:byte-test-base
             i386:byte-sub-base
+            i386:byte-test-base
             i386:call-accu
             i386:call-label
             i386:formal
             i386:function-locals
             i386:function-preamble
-            i386:label-mem-add
-            i386:label->accu
-            i386:label->base
-            i386:label-mem->accu
-            i386:label-mem->base
             i386:jump
             i386:jump
             i386:jump-byte-z
@@ -88,29 +85,36 @@
             i386:jump-ge
             i386:jump-nz
             i386:jump-z
+            i386:label->accu
+            i386:label->base
+            i386:label-mem->accu
+            i386:label-mem->base
+            i386:label-mem-add
             i386:local->accu
             i386:local->base
             i386:local-add
             i386:local-address->accu
+            i386:local-address->accu
+            i386:local-address->base
             i386:local-ptr->accu
             i386:local-ptr->base
-            i386:local-address->base
             i386:local-test
+            i386:mem+n->accu
             i386:mem->accu
             i386:mem->base
-            i386:mem+n->accu
             i386:nop
+            i386:nz->accu
             i386:pop-accu
-            i386:push-accu
             i386:pop-base
+            i386:push-accu
             i386:push-base
+            i386:push-byte-local-de-de-ref
+            i386:push-byte-local-de-ref
             i386:push-label
             i386:push-label-mem
             i386:push-local
-            i386:push-byte-local-de-ref
-            i386:push-byte-local-de-de-ref
-            i386:push-local-de-ref
             i386:push-local-address
+            i386:push-local-de-ref
             i386:ret
             i386:ret-local
             i386:sub-base
@@ -118,17 +122,12 @@
             i386:value->accu
             i386:value->accu-address
             i386:value->accu-address+n
+            i386:value->base
             i386:value->label
             i386:value->local
-            i386:value->base
             i386:xor-accu
             i386:xor-zf
-            i386:accu+n
-            i386:base+n
-            i386:base-address->accu-address
-            i386:nz->accu
             i386:z->accu
-            i386:accu<->stack
             ))
 
 (include-from-path "mes/as-i386.mes")
