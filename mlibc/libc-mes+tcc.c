f29479dfa2e7ba83de400153b3b36089b234ac1b
Jan Nieuwenhuizen <janneke@gnu.org>
2017-07-29 11:09:00 +0200

    mescc: Tinycc support: memcmp.
    * mlibc/libc-mes+tcc.c (memcmp): Implement.

--- mlibc/libc-mes+tcc.c
+++ mlibc/libc-mes+tcc.c
@@ -196,7 +196,10 @@ memset (void *s, int c, size_t n)
 int
 memcmp (void const *s1, void const *s2, size_t n)
 {
-  return 0;
+  char *a = s1;
+  char *b = s2;
+  while (*a == *b && --n) {a++;b++;}
+  return *a - *b;
 }
 
 int
