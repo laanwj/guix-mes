81ddc698bba482f4248e03251221cc7a2330e402
Jan Nieuwenhuizen <janneke@gnu.org>
2017-09-10 21:10:08 +0200

    guix: Bump Nyacc to 0.82.0.
    * guix.scm (nyacc): Bump to 0.82.0.

--- guix.scm
+++ guix.scm
@@ -84,16 +84,15 @@
 (define-public nyacc
   (package
     (name "nyacc")
-    (version "0.80.41")
+    (version "0.82.0")
     (source (origin
               (method url-fetch)
-              (uri (string-append "https://gitlab.com/janneke/nyacc"
-                                  "/repository/archive.tar.gz?ref=v"
-                                  version))
+              (uri (string-append "mirror://savannah/nyacc/"
+                                  name "-" version ".tar.gz"))
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "0x0qff81s3yb30b72j94rj7pnsjafgfp8hkyscymg5438g184gwa"))))
+                "1ll0mjivhxpj3r81w4a8p4bclr3byzp38ig1j11jvwnbl6lawgj7"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("guile" ,guile-2.2)))
