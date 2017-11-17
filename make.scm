02e70486a172c1318703ddd1f8f12404b693e274
Jan Nieuwenhuizen <janneke@gnu.org>
2017-11-17 15:24:45 +0100

    mescc: Tinycc support: struct struct array.
    Support
    
      struct foo
      {
        struct bar baz[N];
        struct bar bla*[M];
      };
    
    * module/language/c99/compiler.mes (expr->accu):
    * scaffold/tests/7n-struct-struct-array.c: Test it.
    * make.scm (add-scaffold-test): Build it.

--- make.scm
+++ make.scm
@@ -245,7 +245,8 @@ exec ${GUILE-guile} --no-auto-compile -L . -L guile -C . -C guile -s "$0" ${1+"$
    "7j-strtoull"
    "7k-for-each-elem"
    "7l-struct-any-size-array"
-   "7m-struct-char-array-assign"))
+   "7m-struct-char-array-assign"
+   "7n-struct-struct-array"))
 
 (add-target (group "check-scaffold-tests/7" #:dependencies (filter (target-prefix? "check-scaffold/tests/7") %targets)))
 
