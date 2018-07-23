f1778a7a0c0c4a883867a67df403e1ae8a18f994
Jan Nieuwenhuizen <janneke@gnu.org>
2018-07-23 19:10:34 +0200

    Add missing copyright headers.
    * .dir-locals.el: Add copyright header.
    * AUTHORS: Likewise.
    * HACKING: Likewise.
    * INSTALL: Likewise.
    * README: Likewise.
    * build-aux/export.make: Likewise.
    * build-aux/setup-mes.sh: Likewise.
    * configure.sh: Likewise.
    * install.sh: Likewise.
    * scaffold/tests/90-goto-var.c: Likewise.
    * scaffold/tests/91-goto-array.c: Likewise.
    * tests/read.test: Likewise.
    * uninstall.sh: Likewise.
    * doc/fosdem/beamercolorthemeX.sty:
    * doc/fosdem/beamerthemeX.sty:
    * doc/fosdem/html.sty: Remove.
    * doc/fosdem/README: New file.
    * doc/images/README: New file.

--- scaffold/tests/91-goto-array.c
+++ scaffold/tests/91-goto-array.c
@@ -1,9 +1,29 @@
+/* -*-comment-start: "//";comment-end:""-*-
+ * GNU Mes --- Maxwell Equations of Software
+ * Copyright © 2018 rain1
+ *
+ * This file is part of GNU Mes.
+ *
+ * GNU Mes is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * GNU Mes is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
 int main(void) {
 	static void *lbls[] = { &&lbl_h, &&lbl_e, &&lbl_l, &&lbl_l, &&lbl_o, &&lbl_quit };
 	static void **lbl = lbls;
-	
+
 	goto **lbl;
 
 lbl_e:
@@ -30,5 +50,3 @@ lbl_quit:
 	return 0;
 
 }
-
-
