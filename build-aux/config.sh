b5a725256c210c19b422bc7ba3e1b9e48edd2eb1
Jan Nieuwenhuizen <janneke@gnu.org>
2018-09-09 07:11:50 +0200

    build: Oops, fix ./check.sh for bootstrap build.
    * build-aux/config.sh (top_builddir): Set default to `.'.

--- build-aux/config.sh
+++ build-aux/config.sh
@@ -17,6 +17,7 @@
 # along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
 
 srcdir=${srcdir-.}
+top_builddir=${top_builddir-.}
 if [ "$V" = 2 ]; then
     echo $0
     echo srcdest=${srcdest}
