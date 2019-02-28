c78a087c38d433627987d8d20720836da046c857
Jan Nieuwenhuizen <janneke@gnu.org>
2019-02-28 10:25:37 +0100

    mescc: ctype.h: Add missing prototypes.
    * include/ctype.h: Add missing prototypes.

--- include/ctype.h
+++ include/ctype.h
@@ -29,11 +29,22 @@
 
 #else // ! WITH_GLIBC
 #include <endian.h>
+
+int isalnum (int c);
 int isalpha (int c);
 int isascii (int c);
+int iscntrl (int c);
 int isdigit (int c);
-int isxdigit (int c);
+int islower (int c);
+int isnumber (int c, int base);
+int isprint (int c);
+int ispunct (int c);
 int isspace (int c);
+int isupper (int c);
+int isxdigit (int c);
+int tolower (int c);
+int toupper (int c);
+
 #endif // ! WITH_GLIBC
 
 #endif // __MES_CTYPE_H
