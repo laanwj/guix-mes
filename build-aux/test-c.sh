39498d92331d16be54eabde528833ff7748971ae
Jan Nieuwenhuizen <janneke@gnu.org>
2019-09-07 19:55:12 +0200

    test: Bump timeout.
    * build-aux/test-c.sh: Bump timeout to 10s, __mesabi_uldiv can be slow.

--- build-aux/test-c.sh
+++ build-aux/test-c.sh
@@ -63,7 +63,7 @@ $CC -c $CPPFLAGS $CFLAGS -o "$o".o "$t"
 $CC $CFLAGS $LDFLAGS -L . -o "$o" $crt1 "$o".o $LIBS
 
 set +e
-timeout 2 "$o" -s --long file0 file1 > "$o".1 2> "$o".2
+timeout 10 "$o" -s --long file0 file1 > "$o".1 2> "$o".2
 r=$?
 set -e
 if [ -f "$b".exit ]; then
