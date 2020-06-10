e9672324f334ffa2f07cc6528eaea867d2a93dd6
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-06-10 15:30:04 +0200

    dir-locals.el: Add indentation for Guix keywords.
    * .dir-locals.el (scheme-mode): Add indentation rules for Guix package
    definitions.

--- .dir-locals.el
+++ .dir-locals.el
@@ -34,6 +34,20 @@
  (scheme-mode
   .
   ((geiser-active-implementations . (guile))
+
+   ;; Guix package
+   (eval . (put 'package 'scheme-indent-function 0))
+   (eval . (put 'origin 'scheme-indent-function 0))
+
+   ;; Guix 'modify-phases' and its keywords.
+   (eval . (put 'modify-phases 'scheme-indent-function 1))
+   (eval . (put 'replace 'scheme-indent-function 1))
+   (eval . (put 'add-before 'scheme-indent-function 2))
+   (eval . (put 'add-after 'scheme-indent-function 2))
+
+   ;; Guix utils
+   (eval . (put 'substitute* 'scheme-indent-function 1))
+
    (eval
     .
     (progn
