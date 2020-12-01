9711ed70ecf0c57e5b75b41b1333f4277ed7df50
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-12-01 11:36:23 +0100

    tests: Add 70-or-argument.c.
    * lib/tests/scaffold/70-or-argument.c: New file.
    * build-aux/check-mescc.sh (tcc_tests): Add it.

--- build-aux/check-mescc.sh
+++ build-aux/check-mescc.sh
@@ -194,6 +194,7 @@ lib/tests/scaffold/70-struct-post.c
 lib/tests/scaffold/70-extern.c
 lib/tests/scaffold/70-ternary-arithmetic-argument.c
 lib/tests/scaffold/70-function-modulo.c
+lib/tests/scaffold/70-or-argument.c
 lib/tests/setjmp/80-setjmp.c
 lib/tests/stdio/80-sscanf.c
 lib/tests/stdlib/80-qsort.c
