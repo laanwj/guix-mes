5eaf1c14ef3fdb3f5e0e88409d10f715f89edcd0
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-12-12 22:52:13 +0100

    mescc: Mes C Library: Declare __mesabi_uldiv.
    * include/mes/lib.h (__mesabi_uldiv): Add prototype.

--- include/mes/lib.h
+++ include/mes/lib.h
@@ -70,6 +70,8 @@ long __mesabi_imod (long a, long b);
 long __mesabi_idiv (long a, long b);
 unsigned long __mesabi_umod (unsigned long a, unsigned long b);
 unsigned long __mesabi_udiv (unsigned long a, unsigned long b);
+unsigned long __mesabi_uldiv (unsigned long a, unsigned long b,
+                              unsigned long *remainder);
 
 void *__memcpy (void *dest, void const *src, size_t n);
 void *__memmove (void *dest, void const *src, size_t n);
