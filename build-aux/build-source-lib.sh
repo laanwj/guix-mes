snnw/mes

ARM: libtcc1: Add div.c, __memcpy, __memmove, __memset.
* build-aux/configure-lib.sh (libtcc1_SOURCES)[arm]: Add div.c,
__memcpy, __memmove, __memset.
* build-aux/build-source-lib.sh: Generate libtcc1.

build-aux/build-source-lib.sh
@@ -49,5 +49,22 @@ for c in $libc_gnu_SOURCES; do
 done
 cp libc+gnu.c $mes_cpu-mes
 
-cp ${srcdest}lib/libtcc1.c $mes_cpu-mes
+rm -f libtcc1.c
+cat > libtcc1.c <<EOF
+// Generated from Mes -- do not edit
+// compiler: $compiler
+// cpu:      $mes_cpu
+// bits:     $mes_bits
+// libc:     $mes_libc
+// kernel:   $mes_kernel
+// system:   $mes_system
+
+EOF
+for c in $libc_gnu_SOURCES; do
+    echo "// $c" >> libtcc1.c
+    cat ${srcdest}$c >> libtcc1.c
+    echo >> libtcc1.c
+done
+cp libtcc1.c $mes_cpu-mes
+
 cp ${srcdest}lib/posix/getopt.c $mes_cpu-mes/libgetopt.c

laanwj/guix-mes

build-aux/build-source-lib.sh
@@ -49,5 +49,22 @@ for c in $libc_gnu_SOURCES; do
 done
 cp libc+gnu.c $mes_cpu-mes
 
-cp ${srcdest}lib/libtcc1.c $mes_cpu-mes
+rm -f libtcc1.c
+cat > libtcc1.c <<EOF
+// Generated from Mes -- do not edit
+// compiler: $compiler
+// cpu:      $mes_cpu
+// bits:     $mes_bits
+// libc:     $mes_libc
+// kernel:   $mes_kernel
+// system:   $mes_system
+
+EOF
+for c in $libtcc1_SOURCES; do
+    echo "// $c" >> libtcc1.c
+    cat ${srcdest}$c >> libtcc1.c
+    echo >> libtcc1.c
+done
+cp libtcc1.c $mes_cpu-mes
+
 cp ${srcdest}lib/posix/getopt.c $mes_cpu-mes/libgetopt.c