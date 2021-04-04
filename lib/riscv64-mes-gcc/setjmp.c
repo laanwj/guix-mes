f043e5cefcdc5e1b71d3f838fc1fbb78e5027cdc
W. J. van der Laan <laanwj@protonmail.com>
2021-04-04 07:23:13 +0000

    lib: Base riscv64-mes-gcc support (dummy setjmp).
    * lib/riscv64-mes-gcc/setjmp.c: New file.
    (setjmp, longjmp): Add dummy implementations. Implementing these for gcc
    would be very involved, and does not seem necessary to get mescc to
    work.

---
+++ lib/riscv64-mes-gcc/setjmp.c
@@ -0,0 +1,40 @@
+/* -*-comment-start: "//";comment-end:""-*-
+ * GNU Mes --- Maxwell Equations of Software
+ * Copyright © 2017,2018,2019 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ * Copyright © 2021 W. J. van der Laan <laanwj@protonmail.com>
+ *
+ * This file is part of GNU Mes.
+ *
+ * GNU Mes is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * GNU Mes is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <setjmp.h>
+#include <stdlib.h>
+
+/* This doesn't do anything at the moment.
+ * According to setjmp.h "It is not supported to use mes' setjmp implementation together with GCC."
+ * We cannot offer this functionality as a C function. It remains to be done (as assembly) for tcc.
+ */
+
+void
+longjmp (jmp_buf env, int val)
+{
+  exit (42);
+}
+
+int
+setjmp (jmp_buf env)
+{
+  return -1;
+}
